67bc7d0d9f260aa1fecbad5e4103a64e0538ef46
blagomail1 <41610337+blagomail1@users.noreply.github.com>
2023-01-09 18:52:49 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
+const hotPrices = document.querySelectorAll('.hot');
+for (let i = 0; i < hotPrices.length; i++) {
+  const hotPrice = hotPrices[i];
+  hotPrice.insertAdjacentText('afterend', '🔥');
+}
+
+
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
