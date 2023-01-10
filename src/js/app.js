39aa7a27e1c5d7d6c714ea490dd6227ee419173f
blagomail1 <41610337+blagomail1@users.noreply.github.com>
2023-01-10 09:36:50 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
-const hotPrices = document.querySelectorAll('.hot');
-for (let i = 0; i < hotPrices.length; i++) {
-const hotPrice = hotPrices[i];
-hotPrice.innerHTML += "\u1F525";
+const nodeList = document.querySelectorAll(".hot");
+for (let i = 0; i < nodeList.length; i++) {
+nodeList[i].textContent+="🔥";
 }
 
 import "../scss/app.scss";
